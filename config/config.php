227de3757e73e3425b0ffad0c5779391c86adda7
vanhoa-tdc <hoa96610@gmail.com>
2025-10-25 20:48:42 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,12 +2,12 @@
 $configDB = array();
 $configDB["host"] 		= "localhost";
 $configDB["database"]	= "computer_store";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
+$configDB["username"] 	= "webtaixiu";
+$configDB["password"] 	= "tdc@12345";
 define("HOST", "localhost");
 define("DB_NAME", "computer_store");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "webtaixiu");
+define("DB_PASS", "tdc@12345");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
